42895599ab936f66c66c4f44e4ada35435f9db2a
Afia Tasnim <77075953+afia25@users.noreply.github.com>
2025-08-08 18:05:02 +0600

    Update Education.jsx

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -10,7 +10,7 @@ const Education = () => {
 
   return (
     <div className="border-b border-neutral-900 pb-4">
-      <h2 className="my-20 text-center text-4xl">Educational</h2>
+      <h2 className="my-20 text-center text-4xl">Education</h2>
       <div>
         {displayedEducation.map((edu, index) => (
           <div key={index} className="mb-8 flex flex-wrap lg:justify-center">
