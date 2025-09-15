1642547e17dccc136ab7bf5077f30a89a3cec336
afia25 <18201033@uap-bd.edu>
2025-09-16 01:04:29 +0600

    portfolio

---
+++ src/components/Technologies.jsx
@@ -0,0 +1,251 @@
+// languages
+import { AiFillCopyrightCircle } from "react-icons/ai";
+import { BiLogoCPlusPlus } from "react-icons/bi";
+import { DiJava } from "react-icons/di";
+import { DiPython } from "react-icons/di";
+import { DiDjango } from "react-icons/di";
+import { DiAngularSimple } from "react-icons/di";
+import { BsBootstrap } from "react-icons/bs";
+import { DiHtml5 } from "react-icons/di";
+import { DiCss3 } from "react-icons/di";
+import { DiMysql } from "react-icons/di";
+
+// Tools
+import pycharmImg from "../assets/pycharm.png";
+import eclipseImg from "../assets/eclipse.png";
+import { DiGithubBadge } from "react-icons/di";
+import { DiVisualstudio } from "react-icons/di";
+import { DiIntellij } from "react-icons/di";
+import oracleImg from "../assets/oracle.png";
+
+
+//motion
+import { animate, motion } from "framer-motion";
+
+const iconVariants = (duration) => ({
+    initial: {y: -10},
+    animate: {
+        y: [10, -10],
+        transition: {
+            duration: duration,
+            ease: "linear",
+            repeat: Infinity,
+            repeatType: "reverse",
+        },
+    },
+});
+
+const Technologies = () => {
+  return (
+    <div className="border-b border-neutral-800 pb-24">
+        <h1 className="my-10 text-center text-4xl">Skills</h1>
+        <motion.h2 
+        whileInView={{opacity: 1, y: 0}}
+        initial={{opacity: 0, y: 100}}
+        transition={{duration: 1.5}}
+        className="my-8 text-center text-2xl">
+            Languages
+        </motion.h2>
+        <motion.div 
+        whileInView={{opacity: 1, x: 0}}
+        initial={{opacity: 0, x: -100}}
+        transition={{duration: 1.5}}
+        className="flex flex-wrap items-center justify-center gap-4">
+
+            <a href="https://www.geeksforgeeks.org/c-programming-language/" target="_blank"
+                    rel="noopener noreferrer" className="inline-block">
+            <motion.div
+            variants={iconVariants(2)}
+            initial="initial"
+            animate="animate" 
+            className="rounded-2xl border-4 border-neutral-800 p-4">
+                    <AiFillCopyrightCircle className="text-7xl text-cyan-400" />
+            </motion.div></a>
+
+            <a href="https://isocpp.org/" target="_blank"
+                    rel="noopener noreferrer" className="inline-block">
+            <motion.div
+            variants={iconVariants(4)}
+            initial="initial"
+            animate="animate" 
+            className="rounded-2xl border-4 border-neutral-800 p-4">
+                <BiLogoCPlusPlus className="text-7xl text-blue-300"/>
+            </motion.div></a>
+
+            <a href="https://www.oracle.com/java/" target="_blank"
+                    rel="noopener noreferrer" className="inline-block">
+            <motion.div 
+            variants={iconVariants(2)}
+            initial="initial"
+            animate="animate"
+            className="rounded-2xl border-4 border-neutral-800 p-4">
+                <DiJava className="text-7xl text-red-500"/>
+            </motion.div></a>  
+
+            <a href="https://www.python.org/" target="_blank"
+                    rel="noopener noreferrer" className="inline-block">
+            <motion.div
+            variants={iconVariants(4)}
+            initial="initial"
+            animate="animate"
+            className="rounded-2xl border-4 border-neutral-800 p-4">
+                <DiPython className="text-7xl text-yellow-400"/>
+            </motion.div></a>
+
+            <a href="https://www.djangoproject.com/" target="_blank"
+                    rel="noopener noreferrer" className="inline-block">
+            <motion.div 
+            variants={iconVariants(2)}
+            initial="initial"
+            animate="animate"
+            className="rounded-2xl border-4 border-neutral-800 p-4">
+                <DiDjango className="text-7xl text-green-400"/>
+            </motion.div></a>
+
+            <a href="https://angular.io/" target="_blank"
+                    rel="noopener noreferrer" className="inline-block">
+            <motion.div 
+            variants={iconVariants(4)}
+            initial="initial"
+            animate="animate"
+            className="rounded-2xl border-4 border-neutral-800 p-4">
+                <DiAngularSimple className="text-7xl text-red-400"/>
+            </motion.div></a>
+
+            <a href="https://getbootstrap.com/" target="_blank"
+                    rel="noopener noreferrer" className="inline-block">
+            <motion.div 
+            variants={iconVariants(2)}
+            initial="initial"
+            animate="animate"
+            className="rounded-2xl border-4 border-neutral-800 p-4">
+                <BsBootstrap className="text-7xl text-green-400"/>
+            </motion.div></a>
+
+            <a href="https://developer.mozilla.org/en-US/docs/Web/Guide/HTML/HTML5" target="_blank"
+                    rel="noopener noreferrer" className="inline-block">
+            <motion.div 
+            variants={iconVariants(4)}
+            initial="initial"
+            animate="animate"
+            className="rounded-2xl border-4 border-neutral-800 p-4">
+                <DiHtml5 className="text-7xl text-orange-400"/>
+            </motion.div></a>
+
+            <a href="https://developer.mozilla.org/en-US/docs/Web/CSS" target="_blank"
+                    rel="noopener noreferrer" className="inline-block">
+            <motion.div 
+            variants={iconVariants(2)}
+            initial="initial"
+            animate="animate"
+            className="rounded-2xl border-4 border-neutral-800 p-4">
+                <DiCss3 className="text-7xl text-blue-400"/>
+            </motion.div></a>
+
+            <a href="https://www.mysql.com/" target="_blank"
+                    rel="noopener noreferrer" className="inline-block">
+            <motion.div 
+            variants={iconVariants(4)}
+            initial="initial"
+            animate="animate"
+            className="rounded-2xl border-4 border-neutral-800 p-4">
+                <DiMysql className="text-7xl text-cyan-400"/>
+            </motion.div></a>
+
+        </motion.div>
+
+        <motion.h2 
+        whileInView={{opacity: 1, y: 0}}
+        initial={{opacity: 0, y: -100}}
+        transition={{duration: 1.5}}
+        className="my-8 text-center text-2xl">
+            Familiar Tools
+        </motion.h2>
+
+        
+        <motion.div
+        whileInView={{opacity: 1, x: 0}}
+        initial={{opacity: 0, x: 100}}
+        transition={{duration: 1.5}}
+        className="flex flex-wrap items-center justify-center gap-4">
+
+            <a href="https://www.jetbrains.com/pycharm/" target="_blank"
+                    rel="noopener noreferrer" className="inline-block">
+            <motion.div 
+            variants={iconVariants(4)}
+            initial="initial"
+            animate="animate"
+            className="rounded-2xl border-4 border-neutral-800 p-4 flex items-center justify-center h-[112px] w-[112px]"> 
+                <img 
+                src={pycharmImg}
+                className="max-w-[68px] max-h-[64px] object-contain" 
+                alt="PyCharm"
+              />    
+            </motion.div></a>
+
+            <a href="https://www.eclipse.org/" target="_blank"
+                    rel="noopener noreferrer" className="inline-block">
+            <motion.div 
+            variants={iconVariants(2)}
+            initial="initial"
+            animate="animate"
+            className="rounded-2xl border-4 border-neutral-800 p-4 flex items-center justify-center h-[112px] w-[112px]">  
+                <img 
+                src={eclipseImg}
+                className="max-w-[68px] max-h-[64px] object-contain"
+                alt="eclipse"
+              />    
+            </motion.div></a>
+
+            <a href="https://github.com/" target="_blank"
+                    rel="noopener noreferrer" className="inline-block">
+            <motion.div 
+            variants={iconVariants(4)}
+            initial="initial"
+            animate="animate"
+            className="rounded-2xl border-4 border-neutral-800 p-4">
+                <DiGithubBadge className="text-7xl text-white-500"/>
+            </motion.div></a>  
+
+            <a href="https://code.visualstudio.com" target="_blank"
+                    rel="noopener noreferrer" className="inline-block">
+            <motion.div 
+            variants={iconVariants(2)}
+            initial="initial"
+            animate="animate"
+            className="rounded-2xl border-4 border-neutral-800 p-4">
+                <DiVisualstudio className="text-7xl text-blue-500"/>
+            </motion.div></a>
+
+            <a href="https://www.jetbrains.com/idea/" target="_blank"
+                    rel="noopener noreferrer" className="inline-block">
+            <motion.div 
+            variants={iconVariants(4)}
+            initial="initial"
+            animate="animate"
+            className="rounded-2xl border-4 border-neutral-800 p-4">
+                <DiIntellij className="text-7xl text-white-400"/>
+            </motion.div></a>
+            
+            <a href="https://www.oracle.com/database/technologies/appdev/sql.html" target="_blank"
+                    rel="noopener noreferrer" className="inline-block">
+            <motion.div 
+            variants={iconVariants(2)}
+            initial="initial"
+            animate="animate"
+            className="rounded-2xl border-4 border-neutral-800 p-4 flex items-center justify-center h-[112px] w-[112px]">  
+                <img 
+                src={oracleImg}
+                className="max-w-[68px] max-h-[64px] object-contain"
+                alt="oracle"
+              />    
+            </motion.div></a>
+
+        </motion.div>
+       
+    </div>
+    
+  );
+};
+
+export default Technologies;
---
+++ src/constants/index.js
@@ -0,0 +1,241 @@
+import project1 from "../assets/projects/project-1.png";
+import project2 from "../assets/projects/project-2.jpg";
+import project3 from "../assets/projects/project-3.jpeg";
+import project4 from "../assets/projects/project-4.png";
+import project5 from "../assets/projects/project-5.jpg";
+import project6 from "../assets/projects/project-6.jpg";
+import project7 from "../assets/projects/project-7.jpg";
+import project8 from "../assets/projects/project-8.png";
+
+export const HERO_CONTENT = `Welcome to my portfolio! I'm Afia, a Competitive Programmer with a passion for Programming. This space is dedicated to showcasing my work, skills, and creative journey. Here, you'll find a collection of my projects, each reflecting my dedication and expertise. Whether you're here to explore my portfolio or get in touch, I hope you find something that inspires you. Thank you for visiting, and I look forward to connecting with you!`;
+
+export const ABOUT_TEXT = `Welcome to my portfolio! I am a highly motivated and dedicated individual with a passion for continuous learning and personal development. With a strong work ethic and commitment to achieving my goals, I thrive on taking on new challenges and quickly adapting to diverse environments.`;
+export const ABOUT_TEXT2 = `In my free time, I enjoy reading, exercising, and spending quality time with friends and family. I am always seeking new adventures and experiences that broaden my perspective and enrich my personal growth.`;
+export const ABOUT_TEXT3 = `I am eager to connect with you and welcome any feedback that can help me improve further. Let's engage in a conversation about what makes each of us unique and explore opportunities for growth and collaboration.`;
+
+export const EXPERIENCES = [
+  {
+    year: "March 2025 - July 2025",
+    role: "Research Assistant (Full-Time)",
+    company: "University of Asia Pacific",
+    description: `Conducting research on Convolution Transformer based eye disease detection utilizing Contrastive Learning and attention mechanism, including literature review, data augmentation, experimenting with deep learning and computer vision models, implementing a novel solution, and writing reports.`,
+    technologies: ["Deep Learning", "Computer Vision", "Contrastive Learning", "Vision Transformer", "Attention Mechanism"],
+  },
+  {
+    year: "January 2024 - July 2024\nand July 2024 - January 2025",
+    role: "Teaching Assistant (Full-Time)",
+    company: "University of Asia Pacific.",
+    description: `Assisting teacher in conducting labs, teaching undergraduate-level lab courses (C Programming, Java, Algorithms, DSA), helping students in debugging code, preparing lab questions, and some lab assignments and lab materials, evaluating lab assignments, conducting viva assessments for students.`,
+    technologies: ["Computer Fundamentals", "Algorithms", "Java", "Data Structures"],
+  },
+  {  
+    year: "May 2023 - September 2023",
+    role: "Research Intern (Full-Time)",
+    company: "Advanced Machine Intelligence Research Lab",
+    description: `Conducting literature review on human action detection, data preprocessing, writing reports and experimenting with deep learning and machine learning models.`,
+    technologies: ["Machine Learning", "Deep Learning", "Human Action Recognition"],
+  },
+];
+
+export const PUBLICATIONS = [
+  {
+    year: "18 December, 2024",
+    description: `Revolutionizing rose grading: Real-time detection and accurate assessment with yolov8 and deep learning models.`,
+    publisher: "Springer Nature",
+    journal: "SN Computer Science",
+    role: "First Author",
+    scopus_index: "Q1 | SCImago Index: Q2", 
+    impact_factor: "3.7 | CiteScore: 5.6",   
+    link: "https://link.springer.com/article/10.1007/s42979-024-03556-z"
+  },
+  {
+    year: "September, 2025",
+    description: `PomNet: Diffusion Guided Data Augmentation and Graph Convolution for Enhanced Pomegranate Disease Classification.`,
+    publisher: "IEEE",
+    journal: "IEEE Open Journal of the Computer Society",
+    role: "First Author",
+    scopus_index: "Q1 | SCImago Index: Q1", 
+    impact_factor: "5.7 | CiteScore: 12.6",
+    submitted_paper: "September, 2025"
+  },
+
+];
+
+export const PROGRAMMING = [ 
+  {
+    year: "JUN 2024",
+    team_name: "UAP_Tenacious_Titans",
+    description: `EEE Tech Fest Inter Department Programming Contest 2024`,
+    rank: "6th",
+  },
+
+  {
+    year: "APR 2024",
+    team_name: "UAP_Amogus",
+    description: `IUT 11th National ICT Fest Programming Contest 2024`,
+    rank: "79th",
+  },
+
+  {
+    year: "JAN 2024",
+    team_name: "UAP_Tenacious_Titans",
+    description: `UAP Inter University Collaborative Programming Contest 1.0 (Senior)`,
+    rank: "25th",
+  },
+
+  {
+    year: "MAR 2023",
+    team_name: "UAP_Tenacious_Titans",
+    description: `EEE Tech Fest 1.0 Presents Intra University Programming Contest -2023`,
+    rank: "9th",
+  },
+
+  {
+    year: "Sep 2022",
+    team_name: "UAP_Amogus",
+    description: `Intra University Hackathon - 2022`,
+    rank: "10th",
+  },
+
+  {
+    year: "APR 2022",
+    team_name: "UAP_Trinity",
+    description: `UAP Intra University Programming Contest Junior - 2022`,
+    rank: "7th",
+  },
+
+];
+
+export const EDUCATION = [
+  {
+    GPA: `CGPA: 3.86 / 4.00 (2nd Position in batch)`,
+    year: `Passing Year: 2025`,
+    degree: `B.Sc in Computer Science and Engineering`,
+    institution: `University of Asia Pacific`,
+    other: `Duration: 4 years`,
+  },
+ 
+  {
+    GPA: `GPA: 4.25 (out of 5.00)`,
+    year: `Passing Year: 2018`,
+    degree: `Higher Secondary Certificate (HSC)`,
+    institution: `Holy Cross College`,
+    other: `Group: Science, Board: Dhaka`,
+  },
+
+  {
+    GPA: `GPA: 5.00 (out of 5.00)`,
+    year: `Passing Year: 2016`,
+    degree: `Secondary School Certificate (SSC)`,
+    institution: `Mohammadpur Preparatory School and College`,
+    other: `Group: Science, Board: Dhaka`,
+  },
+];
+
+export const AWARDS = [
+  {
+    year: "First Year First Semester",
+    description: `Dean's Honour Award Fall - 20`,
+  },
+
+  {
+    year: "First Year Second Semester",
+    description: `Vice Chancellor's Honor Award Spring - 21`,
+  },
+
+  {
+    year: "Second Year First Semester",
+    description: `Dean's Honour Award Fall - 21`,
+  },
+
+  {
+    year: "Second Year Second Semester",
+    description: `Dean's Honour Award Spring - 22`,
+  },
+
+  {
+    year: "Third Year First Semester",
+    description: `Dean's Honour Award Fall - 22`,
+  },
+
+  {
+    year: "Third Year Second Semester",
+    description: `Dean's Honour Award Spring - 23`,
+  },
+
+  {
+    year: "Fourth Year First Semester",
+    description: `Dean's Honour Award Fall - 23`,
+  },
+
+  {
+    year: "Fourth Year Second Semester",
+    description: `Vice Chancellor's Honor Award Spring - 24`,
+  },
+
+];
+
+export const PROJECTS = [
+  {
+    title: "Crowdfunding Platform with Integrated Software Testing",
+    image: project2,
+    description: 
+      "A smart blockchain-based crowdfunding platform (Web Application) can become a source of money for a project by asking many contributors to donate a small amount to it individually. People using this platform can crowdfund for just about anything.",
+    technologies: ["Python", "Django", "MySQL", "Blockchain", "Selenium", "HTML", "CSS"],
+    link: "https://github.com/afia25/Crowdfunding-Platform-with-Integrated-Software-Testing",
+  },
+  {
+    title: "Machine Learning Based Diabetes Prediction and Doctor Appointment System",
+    image: project1,
+    description: 
+      "A Machine Learning Based Diabetes Prediction and Doctor Appointment System predicts diabetes risk using patient data and allows users to book doctor appointments. Features include user registration, health data input, prediction results, appointment scheduling, cancelling appointment.",
+    technologies: ["Machine Learning", "Django", "MySQL", "Python", "HTML", "CSS"],
+    link: "https://github.com/afia25/Machine-Learning-Based-Diabetes-Prediction-and-Doctor-Appointment-System",
+  },
+  {
+    title: "Inventory Management System with Automated Web Testing using Python and Selenium",
+    image: project4,
+    description: 
+      "An Inventory Management System with Automated Web Testing streamlines product tracking, stock updates, supplier management, and sales monitoring. The system ensures accuracy and efficiency through real-time inventory control and automated testing to validate core functionalities across workflows.",
+    technologies: ["Selenium", "MySQL", "Python", "Django", "HTML", "CSS"],
+    link: "https://github.com/afia25/Inventory-Management-System-with-Automated-Web-Testing-using-Python-and-Selenium",
+  },
+  {
+    title: "Employee Management System using Java",
+    image: project6,
+    description:   
+      "An Employee Management System using Java manages employee records, attendance, departments, and salary details. It enables functionalities like employee registration, role assignment, performance tracking, and payroll processing, ensuring efficient workforce management.",
+    technologies: ["Java", "Object-Oriented Programming (OOP)", "C++", "CSS"],
+    link: "https://github.com/afia25/Employee-Management-System-using-Java",
+  },
+  {
+    title: "Computer Vision Based Fitness Pose Evaluation",
+    image: project3,
+    description:   
+      "A real-time vision-based system was developed to detect human joints from videos and evaluate fitness using pose estimation techniques, helping users perform exercises correctly by comparing predicted postures with ideal reference models.",
+    technologies: ["Computer Vision", "Machine Learning", "Python", "OpenCV"],
+    link: "https://github.com/afia25/Computer-Vision-Based-Fitness-Pose-Evaluation",
+  },
+  {
+    title: "Healthcare Information Management System using SQL",
+    image: project7,
+    description: 
+      "A database system using SQL that manages patient records, doctor details, appointments, and medical histories. It includes multiple tables with relations among them to streamline healthcare operations, ensuring efficient access to critical medical information.",
+    technologies: ["MySQL", "XAMPP", "ER Diagram Tool"],
+    link: "https://github.com/afia25/Healthcare-Information-Management-System-using-SQL",
+  },
+  {
+    title: "Create Read Update Delete MVT",
+    image: project8,
+    description:   
+      "A CRUD-based web application using Django’s MVT architecture that allows users to create, view, update, and delete records. It includes functionalities such as data entry forms, dynamic record display, real-time editing, and deletion with confirmation message.",
+    technologies: ["JavaScript", "MySQL", "Python", "Django", "HTML", "CSS"],
+    link: "https://github.com/afia25/Create-read-update-delete-mvt-",
+  },
+];
+
+export const CONTACT = {
+  address: "46/C, Asad Avenue, Mohammadpur, Dhaka-1207",
+  phoneNo: "+88 01782368594",
+  email: "afia6402@gmail.com",
+};
