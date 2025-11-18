d8fc8f32e90e6be5bdab326f320515cbaa189376
Afia Tasnim <77075953+afia25@users.noreply.github.com>
2025-11-18 17:42:31 +0600

    Update Technologies.jsx

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -1,4 +1,4 @@
-// languages
+// Technical Skills
 import { AiFillCopyrightCircle } from "react-icons/ai";
 import { BiLogoCPlusPlus } from "react-icons/bi";
 import { DiJava } from "react-icons/di";
@@ -44,7 +44,7 @@ const Technologies = () => {
         initial={{opacity: 0, y: 100}}
         transition={{duration: 1.5}}
         className="my-8 text-center text-2xl">
-            Languages
+            Technical Skills
         </motion.h2>
         <motion.div 
         whileInView={{opacity: 1, x: 0}}
